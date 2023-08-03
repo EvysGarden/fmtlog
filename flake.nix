edb8c4db7aff78114e1ef650122f6e018d00d651
Evy Garden <evysgarden@protonmail.com>
2023-08-03 19:58:01 +0200

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "fmtlog";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+  };
+
+  outputs = { self, nixpkgs, fmtlog, ... }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
+        name = "fmtlog";
+        buildInputs = with pkgs; [
+          fmt
+          cmake
+        ];
+        src = self;
+        buildPhase = ''
+          cmake .
+          cmake --build .
+        '';
+        installPhase = ''
+          mkdir -p $out/lib/
+          cp ./libfmtlog-shared.so $out/lib/libfmtlog-shared.so
+          cp ./libfmtlog-static.a $out/lib/libfmtlog-shared.a
+        '';
+      };
+    };
+}
